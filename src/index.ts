d41921351aa3996ae077245daf76796bcb9f02e4
samstha07 <samshrestha85@gmail.com>
2023-03-12 14:40:09 +0545

    added eslint, prettier and path alias

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+import 'module-alias/register';
 import express, { Request, Response } from 'express';
+import add from '@/math/math';
 
 const app = express();
 
+console.log(add(5, 9));
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('hello from server helpppp');
+  res.send('hello from server asdasdas');
 });
 
-const port: number = 8000;
+const port = 8000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
