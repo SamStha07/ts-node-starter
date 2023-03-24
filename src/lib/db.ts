ba3aeac9a5a9923b5059444e32094112395d0c4b
samstha07 <samshrestha85@gmail.com>
2023-03-24 22:48:15 +0545

    added db url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,20 @@
 import mysql from 'mysql2';
 
-const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '*BzL23%SF6kk',
-  database: 'portfolio',
-});
+// const db = mysql.createConnection({
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: '*BzL23%SF6kk',
+//   database: 'portfolio',
+// });
+// const db = mysql.createConnection({
+//   host: 'containers-us-west-145.railway.app',
+//   user: 'root',
+//   password: 'YFAzGUGToqiW3XvgtAia',
+//   database: 'railway',
+// });
+const db = mysql.createConnection(
+  'mysql://root:YFAzGUGToqiW3XvgtAia@containers-us-west-145.railway.app:7734/railway'
+);
 
 db.connect(err => {
   if (err) {
